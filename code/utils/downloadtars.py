294e752be92789ba583341889336b27a0f0c8122
James Mete <jmete3@gmail.com>
2022-02-24 01:24:14 +0300

    Added if name = main for downloadtars.py

--- code/utils/downloadtars.py
+++ code/utils/downloadtars.py
@@ -12,34 +12,39 @@ import glob
 import ssl
 import yaml
 
-config = yaml.safe_load(open('../../config.yml'))
 
+def downloadtars():
 
+    # Get config yaml for path variable
+    config = yaml.safe_load(open('../../config.yml'))
 
-#get past certificate verified failed issue
-ssl._create_default_https_context = ssl._create_unverified_context
+    #get past certificate verified failed issue
+    ssl._create_default_https_context = ssl._create_unverified_context
 
-print('Beginning file downloads with wget module')
-numfiles = 5000
-baseurl = "https://archive.org/download/Youtube_metadata_02_2019/"
-storelocation = config["config"]["storepath"]
-print(f"Store Location is: {storelocation}")
+    print('Beginning file downloads with wget module')
+    numfiles = 5000
+    baseurl = "https://archive.org/download/Youtube_metadata_02_2019/"
+    storelocation = config["config"]["storepath"]
+    print(f"Store Location is: {storelocation}")
 
-# Check for existing files so we don't redownload the same files
-existing_file_paths = glob.glob(storelocation+"*.tar")
-existing_files = [f[-8:-4] for f in existing_file_paths]
-print(existing_files)
+    # Check for existing files so we don't redownload the same files
+    existing_file_paths = glob.glob(storelocation+"*.tar")
+    existing_files = [f[-8:-4] for f in existing_file_paths]
+    print(existing_files)
 
-for i in range(numfiles):
-    # Due to the enormous amount of data, we will only download every 5th file to get a more diverse but large dataset
-    if i % 5 == 0:
-        filenumber = str(i).zfill(4)
-        print(filenumber)
-        if filenumber not in existing_files:
-            durl = baseurl + filenumber + ".tar"
-            print(f"Downloading {durl}")
-            fileurl = storelocation + filenumber + ".tar"
-            wget.download(durl, fileurl)
-            print(f"Finished downloading {filenumber}")
+    for i in range(numfiles):
+        # Due to the enormous amount of data, we will only download every 5th file to get a more diverse but large dataset
+        if i % 5 == 0:
+            filenumber = str(i).zfill(4)
+            print(filenumber)
+            if filenumber not in existing_files:
+                durl = baseurl + filenumber + ".tar"
+                print(f"Downloading {durl}")
+                fileurl = storelocation + filenumber + ".tar"
+                wget.download(durl, fileurl)
+                print(f"Finished downloading {filenumber}")
 
-print("Finished downloading")
+    print("Finished downloading")
+
+if __name__ == "__main__":
+    downloadtars()
